4b3f9afc916bd943117b420724cf249613d59429
Phillip <phillipmay@videotron.ca>
2018-08-26 22:49:16 -0400

    Add files via upload

---
+++ School/Introduction to Microprocessors/Lab 4/Lab4.X/main.asm
@@ -0,0 +1,94 @@
+#include "p16f887.inc"
+
+; CONFIG1
+; __config 0x33F5
+ __CONFIG _CONFIG1, _FOSC_INTRC_CLKOUT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_ON & _IESO_OFF & _FCMEN_OFF & _LVP_ON
+; CONFIG2
+; __config 0x3FFF
+ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
+
+ ;Variable definint*****************************
+ cblock 0x20
+ counter1		    ;Used for inner loop
+ counter2		    ;Use for outer loop
+ endc
+ ;**********************************************
+
+
+org		00		    ;Fix the debugger
+
+START
+    
+    ;Start of Part2 modification (datasheet page 64)
+    BANKSEL	OSCCON		    ;
+    MOVLW	b'00001000'	    ;
+    MOVWF	OSCCON		    ;
+    ;End of Part2 modification 
+ 
+ 
+    ;Start of port setup
+    BANKSEL	PORTA		    ;Select the bank of PORTA
+    CLRF	PORTA		    ;Initialise PORTA
+    BANKSEL	ANSEL		    ;
+    MOVLW	b'00000000'	    ;
+    MOVWF	ANSEL		    ;0 means digital
+    BANKSEL	TRISA		    ;Select the bank of TRISA
+    MOVLW	b'00000000'
+    MOVWF	TRISA		    ;Set port A0 to output, correct 
+
+    BANKSEL	ANSELH		    ;Set all of port A to digital IO
+    MOVLW	b'00000000'	    ;
+    MOVWF	ANSELH		    ;0 means digital
+  
+    Mainstart:
+    INCF    PORTA
+    CALL    Delaysubrouter   
+    GOTO    Mainstart
+    
+;Subroutines*****************************************************************
+    Delaysubrouter:
+    
+
+    movlw	b'00000010'
+    movwf	counter2	    ;Set the outer loop to loop twice
+    
+    LEDoncycleouter:    
+    BANKSEL	0		    ;Counter is stored in bank0
+    movlw	h'FF'
+    movwf	counter1	    ;Set the counter to loop 255 times
+    
+    
+	LEDoncycle:
+	decfsz	counter1	    ;
+	GOTO	LEDoncycle	    ;Repeat cycle 255 times
+
+    decfsz	counter2	    ;
+    GOTO	LEDoncycleouter     ;Repeat cycle 2 times	
+    
+    ;Start of off cycle
+
+    
+    movlw	b'00000010'
+    movwf	counter2	    ;Set the counter for the outer loop to 2     
+    
+    ;LED OFF cycle outer start
+    LEDoffcycleouter:
+    
+    BANKSEL	0		    ;Counter is stored in bank0
+    movlw	h'FF'
+    movwf	counter1	    ;Set the counter to loop 255 times
+    
+	;LED off inner part
+	LEDoffcycleinner:
+	decfsz	counter1	    ;
+	GOTO	LEDoffcycleinner    ;Repeat cycle 255 times
+    
+    decfsz	counter2	    ;
+    GOTO	LEDoffcycleouter    ;Repeat cycle 255 times
+    	
+    RETURN			    ;Return from the delay subroutine
+    ;GOTO	Mainloopstart	    ;Repeat the on off cycle 
+     
+    GOTO $                          ; loop forever
+
+END
